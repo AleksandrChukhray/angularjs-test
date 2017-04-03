d35f91bdbd537dafa3857df86ebdf9d20c2a53a4
Aleksander Chukhrai <zimmalex6@gmail.com>
2017-04-03 18:54:45 +0300

    init commit

---
+++ app/app.module.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// Define the `myapp` module
+var app = angular.module('myapp', [
+  'ui.bootstrap'
+]);
+
+app.controller('MainCtrl', function($scope){
+  console.log('hello !');
+});
+
+app.controller('CarouselDemoCtrl', function($scope){
+  $scope.myInterval = 5000;
+  $scope.noWrapSlides = false;
+  $scope.active = 0;
+
+  var slides = $scope.slides = [],
+      currIndex = 0;
+
+  $scope.addSlide = function() {
+    var newWidth = 600 + slides.length + 1;
+    slides.push({
+      image: '//unsplash.it/' + newWidth + '/300',
+      text: ['Nice image','Awesome photograph','That is so cool','I love that'][slides.length % 4],
+      id: currIndex++
+    });
+  };
+
+
+  for (var i = 0; i < 4; i++) {
+    $scope.addSlide();
+  }
+
+});
+
